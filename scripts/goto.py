b31ad5044a894e33ad330fb0d01fa9e116cfddeb
Artyom Fedenka <artyom.fedenka@transferwise.com>
2016-03-08 13:35:07 +0200

    Added shell script to project

--- scripts/goto.py
+++ scripts/goto.py
@@ -9,12 +9,12 @@ import subprocess
 @click.argument('arg1', default='help')
 @click.argument('arg2', nargs=-1)
 def cli(arg1, arg2):
-    subprocess.Popen('cd /Applications', shell=True, executable="/bin/zsh")
-    # if len(arg2) > 1:
-    #     keyword, path = arg2[0], arg2[1]
-    # else:
-    #     keyword, path = arg2[0], ''
-    #
-    # if arg1 != '':
-    #     keywordClass = Keyword(arg1)
-    #     result = Main.execute(keywordClass, keyword, path)
+    if len(arg2) > 1:
+        keyword, path = arg2[0], arg2[1]
+    else:
+        keyword, path = arg2[0], ''
+    if arg1 != '':
+        keywordClass = Keyword(arg1)
+        result = Main.execute(keywordClass, keyword, path)
+        if result != True:
+            sys.stdout.write(result)
